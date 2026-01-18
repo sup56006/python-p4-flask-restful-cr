b073b58544fc2fd3046cac403b8e90dce40912ca
sup56006 <isaackyuma56@gmail.com>
2026-01-18 23:40:42 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -16,8 +16,47 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class Home(Resource):
-    pass
+    def get(self):
+        response = make_response(
+            {"message": "Welcome to the Newsletter RESTful API"},
+            200
+        )
+        return response
+
+
+api.add_resource(Home, '/')
+
+
+class Newsletters(Resource):
+    def get(self):
+        newsletters = [n.to_dict() for n in Newsletter.query.all()]
+        return make_response(newsletters, 200)
+
+    def post(self):
+        new_newsletter = Newsletter(
+            title=request.form['title'],
+            body=request.form['body']
+        )
+
+        db.session.add(new_newsletter)
+        db.session.commit()
+
+        return make_response(new_newsletter.to_dict(), 201)
+
+
+api.add_resource(Newsletters, '/newsletters')
+
+
+class NewsletterByID(Resource):
+    def get(self, id):
+        newsletter = Newsletter.query.filter_by(id=id).first()
+        return make_response(newsletter.to_dict(), 200)
+
+
+api.add_resource(NewsletterByID, '/newsletters/<int:id>')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
